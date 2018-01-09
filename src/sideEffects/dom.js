6d07fb3c654598d2e0703ce42faff6fe826fefb3
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2018-01-09 20:31:42 +0100

    feat(dom side effect): added ability to declartively get event observables from dom (heavilly cycle.js inspired api) it works !

--- src/sideEffects/dom.js
+++ src/sideEffects/dom.js
@@ -1,24 +1,11 @@
 const most = require('most')
 const morph = require('morphdom')// require('nanomorph')
+const {proxy} = require('most-proxy')
+const { attach, stream } = proxy()
+// const {holdSubject} = require('../../node_modules/csg-viewer/')
+// require('../observable-utils/most-subject/index')
 
-function domSource () {
-  let storedListeners = []
-  const select = function (query) {
-    let item
-    if ('.' in query) {
-      item = document.getElementsByClassName(query)
-    }
-    if ('#' in query) {
-      item = document.getElementById(query)
-    }
-
-    if (item === undefined) {
-
-    }
-  }
-  return select
-}
-
+const out$ = stream
 function domSink (outToDom$) {
   let tree
   const firstRender$ = outToDom$
@@ -33,14 +20,68 @@ function domSink (outToDom$) {
       morph(tree, newTree)
     })
 
-  return most.mergeArray([
+  const foo$ = most.mergeArray([
     firstRender$,
     otherRenders$
-  ]).forEach(x => x)
+  ]).multicast()
+
+  attach(foo$)
+  foo$.forEach(x => x)
+}
+
+/*let storedObservables = {}
+let waitingForListeners = []
+let eventsForListners = {} */
+let storedListeners = {
+
+}
+function domSource () {
+  function getElement (query) {
+    let item = document.querySelectorAll(query)
+    return item
+  }
+
+  const select = function (query) {
+    console.log('selecting', query)
+    const item = getElement(query)
+
+    let outputStream
+    if (!item || (item && item.length === 0)) {
+      // waitingForListeners.push(query)
+      const eventProxy = proxy()
+      outputStream = eventProxy.stream
+      // storedObservables[query] = eventProxy
+      storedListeners[query] = {observable: eventProxy, live: false}
+    }
+
+    return {events: function events (eventName) {
+      // eventsForListners[query] = eventName
+      storedListeners[query].events = eventName
+      return outputStream
+    }}
+  }
+
+  out$.forEach(function () {
+    console.log('dom source watching dom change')
+    Object.keys(storedListeners).forEach(function (query) {
+      const item = getElement(query)
+      if (item) {
+        const storedListener = storedListeners[query]
+        if (item.length === 1 && storedListener.live === false) {
+          console.log('HURRAY NOW I HAVE SOMETHING !!')          
+          const realObservable = most.fromEvent(storedListener.events, item[0])
+          storedListener.observable.attach(realObservable)
+          storedListener.live = true
+        }
+      }
+    })
+  })
+
+  return {select}
 }
 
 function makeDomSource () {
 
 }
 
-module.exports = {makeDomSource, domSink}
+module.exports = {domSource, domSink}
