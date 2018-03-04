38933a2d168a0e7b7abd0847050ac4cad4eaec6f
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2018-03-04 09:36:28 +0100

    feat(solidsWorker): * added support for VTREE api & caching system * seperated messages for script parameters/definitions & solids

--- src/core/code-evaluation/rebuildSolidsWorker.js
+++ src/core/code-evaluation/rebuildSolidsWorker.js
@@ -1,9 +1,16 @@
+//
+const makeBuildCachedGeometryFromTree = require('jscad-tree-experiment').buildCachedGeometry
+console.log('calling this one here')
+const buildCachedGeometryFromTree = makeBuildCachedGeometryFromTree({passesBeforeElimination: 20})
+
 onmessage = function (event) {
   if (event.data instanceof Object) {
     console.log('in web worker')
+
     const {data} = event
     if (data.cmd === 'render') {
       const {source, parameters, mainPath, options} = data
+      // const {vTreeMode:}
       const {isCAG, isCSG} = require('@jscad/csg')
       const {toArray} = require('../../utils/utils')
 
@@ -15,14 +22,29 @@ onmessage = function (event) {
       const paramDefaults = params
       const paramValues = Object.assign({}, paramDefaults, parameters)
       // console.log('paramDefinitions', paramDefinitions, 'paramValues', paramValues)
+      self.postMessage({'type': 'params', paramDefaults, paramValues, paramDefinitions})
 
-      let solids = toArray(scriptRootModule.main(paramValues))
+      /* let solids = toArray(scriptRootModule.main(paramValues))
         .map(function (object) {
           if (isCSG(object) || isCAG(object)) {
             return object.toCompactBinary()
           }
+        }) */
+      let solids
+      let rawResults = toArray(scriptRootModule.main(paramValues))
+      const isSolidResult = (rawResults.length > 0 && (isCSG(rawResults[0]) || isCAG(rawResults[0])))
+      if (isSolidResult) {
+        solids = rawResults
+      } else {
+        solids = buildCachedGeometryFromTree({}, rawResults)
+      }
+      solids = solids
+        .map(object => {
+          if (isCSG(object) || isCAG(object)) {
+            return object.toCompactBinary()
+          }
         })
-      self.postMessage({solids, paramDefaults, paramValues, paramDefinitions})
+      self.postMessage({'type': 'solids', solids})
     }
   }
 }
